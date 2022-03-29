30eb2a6f4250d3fe881b52767c765c6e59141d9c
David Dollar <david@dollar.io>
2022-03-29 10:28:35 -0400

    make host/port configurable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require("@actions/core");
 const { create, globSource } = require("ipfs-http-client");
 
-const ipfs = create({ host: "ipfs.komputing.org", port: 443, protocol: "https" });
+const ipfs = create({ host: core.getInput("host"), port: core.getInput("port"), protocol: "https" });
 
 async function main() {
 	try {
