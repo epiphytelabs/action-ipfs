54b3572f254d702fbbb79471c5ad296b6c48f0b3
David Dollar <david@dollar.io>
2022-03-29 13:14:39 -0400

    update default ipfs gateway

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require("@actions/core");
 const { create, globSource } = require("ipfs-http-client");
 
-const ipfs = create({ host: core.getInput("host"), port: core.getInput("port"), protocol: "https" });
+const ipfs = create({ host: core.getInput("host"), port: core.getInput("port"), protocol: core.getInput("protocol") });
 
 async function main() {
 	try {
