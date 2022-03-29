381e653ef629e3cebfd69509cfc9b61ef4c661e7
David Dollar <david@dollar.io>
2022-03-29 19:41:25 -0400

    support ipfs auth

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const core = require("@actions/core");
 const { create, globSource } = require("ipfs-http-client");
 
-const ipfs = create({ host: core.getInput("host"), port: core.getInput("port"), protocol: core.getInput("protocol") });
-
 async function main() {
 	try {
+		let options = {
+			host: core.getInput("host"),
+			port: core.getInput("port"),
+			protocol: core.getInput("protocol"),
+		};
+
+		const auth = core.getInput("auth");
+
+		if (auth !== "") {
+			options.headers = {
+				authorization: "Basic " + Buffer.from(auth).toString("base64"),
+			};
+		}
+
+		const ipfs = create(options);
+
 		const path = core.getInput("path");
 		const files = globSource(path, { recursive: true });
 		const result = await ipfs.add(files, { pin: true });
